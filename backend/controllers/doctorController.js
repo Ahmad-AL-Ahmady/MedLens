e4a2d95b0b5f34f6d9b509e5972bf3aca7acf6f2
Ahmad-AL-Ahmady <ahmad.hussain.alahmady@gmail.com>
2025-04-28 02:16:14 +0300

    fix: include age and gender in patient details for doctor profile response

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -24,7 +24,7 @@ exports.getMyProfile = catchAsync(async (req, res, next) => {
       options: { sort: { date: 1 }, limit: 10 },
       populate: {
         path: "patient",
-        select: "firstName lastName avatar",
+        select: "firstName lastName avatar age gender",
       },
     })
     .populate({
